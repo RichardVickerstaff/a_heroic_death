0d277688174d71dd1f4626f4fe630e1343965971
Richard Vickerstaff <richardiv.rv@googlemail.com>
2018-03-04 16:27:44 +0000

    Start to style page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,28 @@
-// This is where it all goes :)
+//= require jquery
+
+$(function () {
+  $(".content").click(function () {
+
+    var value = $(this).find(".number").text();
+
+    if (value !== "<") {
+      $(".numberinput").each(function () {
+        var a = $(this).text();
+        if (!a) {
+          $(this).text(value);
+          $(this).addClass("nocircle");
+          return false;
+        }
+      });
+    } else {
+      $($(".numberinput").get().reverse()).each(function () {
+        var a = $(this).text();
+        if (a) {
+          $(this).text("");
+          $(this).removeClass("nocircle");
+          return false;
+        }
+      });
+    }
+  });
+});
